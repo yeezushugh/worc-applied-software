15997cc7293a2315f2e0f5c6191ba5f08088f5fb
yeezushugh <91317911+yeezushugh@users.noreply.github.com>
2021-09-24 16:08:25 +0100

    Add files via upload
    start @24/09/2021

---
+++ workshop1/worksheet1/FishyNote.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace worksheet1
+{
+    partial class FishyNote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.CollapseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 148);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(376, 217);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter your note text here...";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(559, 12);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(30, 28);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "X\r\n";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CollapseButton
+            // 
+            this.CollapseButton.Location = new System.Drawing.Point(12, 17);
+            this.CollapseButton.Name = "CollapseButton";
+            this.CollapseButton.Size = new System.Drawing.Size(96, 34);
+            this.CollapseButton.TabIndex = 2;
+            this.CollapseButton.Text = "Fishy Collapse";
+            this.CollapseButton.UseVisualStyleBackColor = true;
+            this.CollapseButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // FishyNote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(630, 388);
+            this.Controls.Add(this.CollapseButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FishyNote";
+            this.Text = "FishyNote";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button CollapseButton;
+    }
+}
---
+++ workshop1/worksheet1/FishyNotes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace worksheet1
+{
+    public partial class FishyNotes : Form
+    {
+        public FishyNotes()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
